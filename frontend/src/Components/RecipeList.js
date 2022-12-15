8ed67edff6b49b549680d589181d90e482361127
Petra Eriksson <eriksson.petra@gmail.com>
2022-12-15 14:47:13 +0100

    testing components header, footer, nav works

--- frontend/src/Components/RecipeList.js
+++ frontend/src/Components/RecipeList.js
@@ -2,34 +2,37 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { generateRecipe } from "reducers/recipes";
 import { RecipeCard } from "./reusable/RecipeCard";
+import { Header } from "./reusable/Header";
+import { Footer } from "./reusable/Footer";
 
 const RecipeList = () => {
-  const arrayOfRecipes = useSelector((store) => store.recipes.results)
-  console.log('arrayOfRecipes', arrayOfRecipes)
+  const arrayOfRecipes = useSelector((store) => store.recipes.results);
+  console.log("arrayOfRecipes", arrayOfRecipes);
   // const description = useSelector((store) => store.recipes.results[].description)
   // console.log('description', description)
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   useEffect(() => {
     dispatch(generateRecipe());
-  }, [])
+  }, []);
 
-return(
-  <>
-    <h1>List of recipes</h1>
-    {arrayOfRecipes.map((recipe) => {
-    return (
-      //<p>{recipe.name}</p>
-      <RecipeCard
-       key= {recipe.id}
-       name={recipe.name}
-       time={recipe.total_time_minutes}
-       description={recipe.description}
-       img={recipe.thumbnail_url}
-       />
-     )
-    })}
-  </>
-)
-} 
+  return (
+    <>
+      <h1>List of recipes</h1>
+      {arrayOfRecipes.map((recipe) => {
+        return (
+          <>
+            <RecipeCard
+              key={recipe.id}
+              name={recipe.name}
+              time={recipe.total_time_minutes}
+              description={recipe.description}
+              img={recipe.thumbnail_url}
+            />
+          </>
+        );
+      })}
+    </>
+  );
+};
 
 export default RecipeList;
