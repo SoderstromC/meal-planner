e7cf38dd757e0365482e97633855d83a62073545
Annika Gadman <annika.gadman@gmail.com>
2022-12-15 10:17:55 -0600

    recipelist only available when logged in

--- frontend/src/Components/RecipeList.js
+++ frontend/src/Components/RecipeList.js
@@ -3,18 +3,45 @@ import { useDispatch, useSelector } from "react-redux";
 import { generateRecipe } from "reducers/recipes";
 import { RecipeCard } from "./reusable/RecipeCard";
 
+import { useNavigate, Link } from "react-router-dom";
+import user from "reducers/user";
+
 const RecipeList = () => {
   const arrayOfRecipes = useSelector((store) => store.recipes.results)
   console.log('arrayOfRecipes', arrayOfRecipes)
   // const description = useSelector((store) => store.recipes.results[].description)
   // console.log('description', description)
   const dispatch = useDispatch()
+
+  const accessToken = useSelector((store) => store.user.accessToken);
+  const navigate = useNavigate();
+
   useEffect(() => {
-    dispatch(generateRecipe());
+    if (!accessToken) {
+        navigate("/login");
+    }
+}, []);
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+          "Content-Type": "application/json",
+          "Authorization": accessToken
+      }
+  }
+    dispatch(generateRecipe(), options);
   }, [])
+
+  const logOutOnClick = () => {
+    dispatch(user.actions.setAccessToken(null));
+    navigate("/login");
+};
 
 return(
   <>
+    <button type="button">GO BACK TO MAIN</button>
+    <button type="button" onClick={logOutOnClick}>LOG OUT</button>
     <h1>List of recipes</h1>
     {arrayOfRecipes.map((recipe) => {
     return (
