652a9f70989140570980f574120c491a6cb3d43b
Caroline Söderström <caroline.soderstrom@gmail.com>
2023-01-04 10:24:32 +0100

    added a loader to reusable components

---
+++ frontend/src/Components/reusable/LoadingPan.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import Lottie from 'lottie-react'
+import loading from 'lotties/loadingpan'
+
+const LoadingPan = () => <Lottie animationData={loading} loop />
+
+export default LoadingPan
