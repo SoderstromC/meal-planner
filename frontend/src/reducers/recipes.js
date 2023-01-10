3c201faa2d618383309d3f0b55c087f7d2de6b24
Caroline Söderström <caroline.soderstrom@gmail.com>
2023-01-10 11:10:41 +0100

    added new API key to main

--- frontend/src/reducers/recipes.js
+++ frontend/src/reducers/recipes.js
@@ -36,7 +36,7 @@ export const generateRecipe = () => {
     const generate = {
       method: "GET",
       headers: {
-        "X-RapidAPI-Key": "14dcad3da4msh194f38471e51b0fp1d748cjsndf695bebd2fa",
+        "X-RapidAPI-Key": "05ce32baddmsh1dbc226d22c6d58p17606ejsn8489212bcecc",
         "X-RapidAPI-Host": "tasty.p.rapidapi.com",
       },
     };
@@ -56,7 +56,7 @@ export const generateSingle = (recipeId) => {
     const details = {
       method: "GET",
       headers: {
-        "X-RapidAPI-Key": "14dcad3da4msh194f38471e51b0fp1d748cjsndf695bebd2fa",
+        "X-RapidAPI-Key": "05ce32baddmsh1dbc226d22c6d58p17606ejsn8489212bcecc",
         "X-RapidAPI-Host": "tasty.p.rapidapi.com",
       },
     };
