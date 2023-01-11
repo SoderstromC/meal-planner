23cdce8c106aa179320a18e766c0cca74f3f066b
Annika Gadman <annika.gadman@gmail.com>
2023-01-11 12:53:43 -0600

    commented out not used in StartPage

--- frontend/src/Components/StartPage.js
+++ frontend/src/Components/StartPage.js
@@ -1,15 +1,15 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import thoughts from "reducers/thoughts";
-import { API_URL } from "utils/utils";
+// import thoughts from "reducers/thoughts";
+// import { API_URL } from "utils/utils";
 import { useNavigate, Link } from "react-router-dom";
 import { Header } from "./reusable/Header";
 import styled from "styled-components";
 import { InnerWrapper, OuterWrapper } from './reusable/global/Wrappers';
 
 const StartPage = () => {
-    const thoughtItems = useSelector((store) => store.thoughts.items);
-    const dispatch = useDispatch();
+    // const thoughtItems = useSelector((store) => store.thoughts.items);
+    // const dispatch = useDispatch();
     const accessToken = useSelector((store) => store.user.accessToken);
     const navigate = useNavigate();
 
