a912dc4545a40714886ad4a82a688c337fc81a2b
Petra Eriksson <eriksson.petra@gmail.com>
2023-01-12 21:53:09 +0100

    merging ingredients

--- frontend/src/components/reusable/Ingredients.js
+++ frontend/src/components/reusable/Ingredients.js
@@ -1,23 +1,27 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { API_URL } from "utils/utils";
+// import { generateSingle } from "reducers/recipes";
 import styled from "styled-components";
 
 export const Ingredients = ({components}) => {
-
+  // const components = useSelector((store) => store.recipes.components);
+  // const portions = useSelector((store) => store.recipes.sections);
   const userId = useSelector((store) => store.user.userId);
+
+  console.log('components', components)
 
   const buttonClickSave = () => {
     const SAVED_SHOPPINGLIST_URL = API_URL('saveListItem');
 
     let itemsToSave = [];
-
+    console.log('components2', components)
     components.map((component) => {
       itemsToSave.push({
         "raw_text": component.raw_text,
         "id": component.id
-      });
-    });
+      })
+    })
 
     const options = {
       method: "POST",
@@ -29,6 +33,7 @@ export const Ingredients = ({components}) => {
     fetch(SAVED_SHOPPINGLIST_URL, options)
       .then((res) => res.json())
       .then((data) => {
+        //something (function?) to let the user know we have saved to shopping list
         console.log("savedShoppingListData", data);
       })
       .catch((error) => console.error("error2", error));
@@ -41,15 +46,18 @@ export const Ingredients = ({components}) => {
       <AddToShopping type='button' onClick={buttonClickSave}>Add to shoppinglist</AddToShopping>
       </ButtonWrapper>
       <InnerWrapper>
-        {components.map((component) => {
-          return (
-            <>
-              <RecipeWrapper key={component.id}>
-                <p>{component.raw_text}</p>
-              </RecipeWrapper>
-            </>
-          );
-        })}
+      {components.map((component) => {
+        return (
+          <>
+            <RecipeWrapper key={component.id}>
+              <p>{component.raw_text}</p>
+              {/* <p>{component.ingredient.name}</p>
+              <p>{component.measurements[0].unit.name}</p>
+              <p>{component.measurements[0].quantity}</p> */}
+            </RecipeWrapper>
+          </>
+        );
+      })}
       {/* <AddToShopping type='button' onClick={buttonClickSave}>Add to shoppinglist</AddToShopping> */}
       </InnerWrapper>
     </OuterWrapper>
@@ -59,7 +67,7 @@ export const Ingredients = ({components}) => {
 const OuterWrapper = styled.div`
   display: flex;
   flex-direction: column;
-`
+`;
 
 const InnerWrapper = styled.div`
   display: flex;
@@ -69,9 +77,10 @@ const ButtonWrapper = styled.div`
 display: flex;
 flex-direction: row;
 justify-content: space-between;  
+
 h3{
   margin: 10px 0 20px 0;
-`
+`;
 
 const RecipeWrapper = styled.div`
   display: flex-end;
@@ -93,5 +102,15 @@ const AddToShopping = styled.button`
   border-radius: 13px;
   font-size: 13px;
   font-weight: bold;
-  margin: 15px 0;
+  margin: 5px 0;
+
+  &:hover {
+    color: green;
+    background-color: black;
+  }
+
+  &:active {
+    color: red;
+    background-color: black;
+  }
 `;
