6cd8745f7c42a8f5dd30582d07eea59a07604a58
Annika Gadman <annika.gadman@gmail.com>
2023-01-12 10:05:56 -0600

    styled shoppinglist

--- frontend/src/components/RecipeList.js
+++ frontend/src/components/RecipeList.js
@@ -31,7 +31,7 @@ const RecipeList = () => {
       headers: {
           "Content-Type": "application/json",
           "Authorization": accessToken
-      };
+      },
   };
     dispatch(generateRecipeList(), options);
   }, []);
--- frontend/src/components/ShoppingList.js
+++ frontend/src/components/ShoppingList.js
@@ -1,22 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Header } from "./reusable/Header";
 import { API_URL } from "utils/utils";
 import styled from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { InnerWrapper, OuterWrapper } from './reusable/global/Wrappers';
+import { InnerWrapper, OuterWrapper } from "./reusable/global/Wrappers";
+
 import NoIngredients from "./NoIngredients";
-import shopping from 'reducers/shopping';
+import shopping from "reducers/shopping";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 const MyShoppingList = () => {
   const [shoppingList, setShoppingList] = useState([]);
   const [inputValue, setInputValue] = useState("");
-  //const [loading, setLoading] = useState(false);
   const userId = useSelector((store) => store.user.userId);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const buttonClickToggleCheck = (id) => {
-    dispatch(shopping.actions.toggleItem(id))
+    dispatch(shopping.actions.toggleItem(id));
   };
 
   const accessToken = useSelector((store) => store.user.accessToken);
@@ -30,34 +33,34 @@ const MyShoppingList = () => {
   let counter = 0;
 
   const fetchMyShoppingList = () => {
-
-    const MY_SHOPPINGLIST_URL = API_URL(`listItems/${userId}`)
+    // const MY_SHOPPINGLIST_URL = `http://localhost:8090/listItems/${userId}`;
+    const MY_SHOPPINGLIST_URL = API_URL(`listItems/${userId}`);
 
     const options = {
       method: "GET",
       headers: { "Content-Type": "application/json" },
-      };
-      //setLoading(true);
-      fetch(MY_SHOPPINGLIST_URL, options)
-        .then((res) => res.json())
-        .then((data) => {
-          setShoppingList(data.response);
-          console.log("shoppinglist1loading", data.response);
-        })
-        .catch((error) => console.error("error3", error));
-      //.finally(() => setLoading(false))
     };
+
+    //setLoading(true);
+    fetch(MY_SHOPPINGLIST_URL, options)
+      .then((res) => res.json())
+      .then((data) => {
+        setShoppingList(data.response);
+        console.log("shoppinglist1loading", data.response);
+      })
+      .catch((error) => console.error("error3", error));
+    //.finally(() => setLoading(false))
+  };
 
   useEffect(() => {
     fetchMyShoppingList();
   }, []);
 
-
-  // REMOVE INGREDIENT FROM USERS SAVED SHOPPINGLIST
+  /****  REMOVE INGREDIENT FROM USERS SAVED SHOPPINGLIST ****/
 
   const buttonClickRemove = (id) => {
-
-    const REMOVE_INGREDIENT_URL = API_URL('removeIngredient')
+    // const REMOVE_INGREDIENT_URL = `http://localhost:8090/removeIngredient`;
+    const REMOVE_INGREDIENT_URL = API_URL("removeIngredient");
 
     const options = {
       method: "PUT",
@@ -72,70 +75,48 @@ const MyShoppingList = () => {
       })
       .catch((error) => console.error("error3", error));
   };
-
-
-  // UPDATE INGREDIENT FROM USERS SAVED SHOPPINGLIST
-
-  const buttonClickEditIngredient = (id) => {
-    alert("visar inputfield");
-  };
+  /**** UPDATE INGREDIENT FROM USERS SAVED SHOPPINGLIST ****/
 
   const buttonClickExit = (id) => {
     alert("visar rad med item");
   };
 
-  const buttonClickSave = (id) => {
-    const EDIT_INGREDIENT_URL = API_URL('editIngredient');
-
-    const options = {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ userId: userId, id: id, text: inputValue }),
-    };
-    fetch(EDIT_INGREDIENT_URL, options)
-      .then((res) => res.json())
-      .then((data) => {
-        setShoppingList(data.response);
-        console.log("shoppinglistupdated", data.response);
-      })
-      .catch((error) => console.error("error3", error));
-  };
-
   return (
-    <>
-      <Header />
-      {console.log("shoppinglistbefore deciding on what to show", shoppingList)}
-      {shoppingList.length === 0 && <NoIngredients />}
-      {shoppingList.length > 0 && (
+    <OuterWrapper>
+      <InnerWrapper>
+        <Header />
         <ShoppingListContainer>
-          <h1>My Shopping List</h1>
+          <h3>My Shopping List</h3>
+          {console.log("shoppinglistbefore deciding on what to show", shoppingList)}
+          {shoppingList.length === 0 && <NoIngredients />}
+          {shoppingList.length > 0 && (
           <ListWrapper>
             {shoppingList.map((component) => {
               return (
-                <OuterWrapper>
-                  <InnerWrapper>
-                    <ShoppingItemContainer>
-                      <ShoppingItemWrapper>
-                        <CheckBox
-                          type='checkbox'
-                          checked={component.isCompleted}
-                          onChange={() => buttonClickToggleCheck(component.id)}
-                        />
-                        <Item
-                          key={`${counter++}-${component.id}`}>{component.raw_text}</Item>
-                        <div className = "buttonwrapper">
-                        <RemoveItem onClick={() => buttonClickRemove(component.id)}>delete</RemoveItem>
-                        </div>
-                      </ShoppingItemWrapper>
-                    </ShoppingItemContainer>
-                  </InnerWrapper>
-                </OuterWrapper>
+                <ShoppingItemContainer>
+                  <ShoppingItemWrapper>
+                    <CheckBox
+                      type='checkbox'
+                      checked={component.isCompleted}
+                      onChange={() => buttonClickToggleCheck(component.id)}
+                    />
+                    <Item key={`${counter++}-${component.id}`}>
+                      {component.raw_text}
+                    </Item>
+                    <div className='buttonwrapper'>
+                      <RemoveItem onClick={() => buttonClickRemove(component.id)}>
+                        <FontAwesomeIcon className="trash-icon" icon={faTrashCan} />
+                      </RemoveItem>
+                    </div>
+                  </ShoppingItemWrapper>
+                </ShoppingItemContainer>   
               );
             })}
           </ListWrapper>
+          )}
         </ShoppingListContainer>
-      )}
-    </>
+      </InnerWrapper>
+    </OuterWrapper>
   );
 };
 
@@ -143,49 +124,46 @@ export default MyShoppingList;
 
 const ShoppingListContainer = styled.div`
   width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;  
-`;
-
+`
 const ListWrapper = styled.div`
-  text-align: left;
-  border: solid;
-  width: 70%;
-`;
-
+  width: 100%;
+  border: 1px solid #ACACAC;
+  border-radius: 5px;
+  padding: 30px;
+  margin-top: 10px;
+  background-color: #fafafa;
+`
 const ShoppingItemContainer = styled.div`
   margin: 10px 14px 10px 43px;
   display: flex;
   flex-direction: column;
-`;
-
+`
 const ShoppingItemWrapper = styled.div`
   display: flex;
   flex-direction: row;
-  border: solid;
+  border: 1px solid #ACACAC;
   justify-content: flex-start;
   align-items: center;
-  padding: 0 15px 0 15px; 
-  .buttonwrapper{
+  padding: 0 15px 0 15px;
+
+  .buttonwrapper {
     margin-left: auto;
   }
-`;
-
+`
 const Item = styled.div`
   margin: 10px 10px 10px 50px;
-`;
+`
+
 const CheckBox = styled.input`
   cursor: pointer;
   appearance: none;
   margin: 5px;
   font: inherit;
   color: white;
-  width: 2em;
-  height: 2em;
-  border: 0.15em solid black;
+  width: 1.5em;
+  height: 1.5em;
+  border: 0.12em solid black;
+  border-radius: 3px;
   transform: translateY(-0.075em);
   display: grid;
   place-content: center;
@@ -206,11 +184,17 @@ const CheckBox = styled.input`
     width: 1em;
     height: 1em;
   }
-`;
+`
 const RemoveItem = styled.button`
   margin-left: auto;
-  border: solid;
+  border: none;
   height: 25px;
   width: 80px;
   background-color: transparent;
-`;
+  color: red;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.2);
+  }
+`
